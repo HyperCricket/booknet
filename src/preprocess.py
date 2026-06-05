2f49d9af67713bcec32cad1592b67ab835ba41b0
Kevin N <kevinnguyen4221@gmail.com>
2026-06-04 22:51:33 -0500

    added documentation to all methods in preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,14 +15,27 @@ MIN_AMT_USER_REVIEWS = 5
 MIN_AMT_BOOK_REVIEWS = 10
 
 
-# Function to load ratings csv to a dataframe
 def load(path: str = RATINGS_PATH) -> pd.DataFrame:
+    """Function to load ratings csv to a dataframe
+
+    :param path: path to csv file
+    :type path: string
+    :return: Dataframe of the csv file
+    :rtype: pd.DataFrame
+
+    """
     ratings = pd.read_csv(RATINGS_PATH)
     return ratings
 
 
-# Function to filter out any users that have made less than 5 reviews and books that have less than 10 reviews
 def clean(ratings: pd.DataFrame) -> pd.DataFrame:
+    """Filter out any users that have given less than 5 reviews and books that have less than 10 reviews
+
+    :param ratings: Dataframe containing the ratings
+    :type ratings: pd.DataFrame
+    :return: Dataframe of the filtered ratings
+    :rtype: pd.DataFrame
+    """
     user_counts = ratings["user_id"].value_counts()
     book_counts = ratings["book_id"].value_counts()
 
@@ -34,36 +47,39 @@ def clean(ratings: pd.DataFrame) -> pd.DataFrame:
     return filtered_ratings
 
 
-""" 
-    Create the pivot table where:
-    rows = users
-    cols = books
-    index / cells = ratings
-"""
-
-
 def build_matrix(ratings: pd.DataFrame) -> pd.DataFrame:
+    """Create the pivot table where: rows are the users, cols are the books, and the indices are the ratings
+
+    :param ratings: Dataframe containing the ratings
+    :type ratings: pd.DataFrame
+    :return: Dataframe of the pivot table matrix
+    :rtype: pd.DataFrame
+    """
     matrix = ratings.pivot_table(index="user_id", columns="book_id", values="rating")
 
     return matrix
 
 
-""" 
-    Fill NaNs with 0 and convert to scipy sparse matrix to reduce memory consumption 
-"""
-
-
 def to_sparse(matrix: pd.DataFrame) -> csr_matrix:
+    """Fill NaNs with 0 and convert to scipy sparse matrix to reduce memory consumption
+
+    :param matrix: pivot table
+    :type matrix: pd.DataFrame
+    :return: csr_matrix for better memory usage
+    :rtype: csr_matrix
+    """
     sparse = csr_matrix(matrix.fillna(0))
     return sparse
 
 
-"""
-    Run full preprocessing pipeline in this method
-"""
-
-
 def preprocess(path: str = RATINGS_PATH) -> csr_matrix:
+    """Run full preprocessing pipeline in this method
+
+    :param path: path to csv file
+    :type path: string
+    :return: csr_matrix for better memory usage
+    :rtype: csr_matrix
+    """
     ratings = load(RATINGS_PATH)
     filtered_ratings = clean(ratings)
     table = build_matrix(filtered_ratings)
