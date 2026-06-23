79fbe34e0aff561a0502a438b99b993c72defa98
LucaMacesanu <luca.m@utexas.edu>
2026-06-22 19:48:22 -0500

    2nd try didn't work

--- src/model/mf_baseline.py
+++ src/model/mf_baseline.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 
 
 class MatrixFactorization(nn.Module):
@@ -15,8 +16,8 @@ class MatrixFactorization(nn.Module):
             num_embeddings=num_items, embedding_dim=embedding_dim
         )
 
-        self.init.normal_(self.user_embeddings.weight, std=0.1)
-        self.init.normal_(self.item_embeddings.weight, std=0.1)
+        init.normal_(self.user_embeddings.weight, std=0.1)
+        init.normal_(self.item_embeddings.weight, std=0.1)
 
     def forward(self, users: torch.Tensor, items: torch.Tensor) -> torch.Tensor:
         """Perform one dot product at a time, element wise."""
